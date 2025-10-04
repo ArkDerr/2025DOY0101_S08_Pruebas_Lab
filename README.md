f309509f812ba394085390ee150a8373fa51c8a0
Daniel Riquelme <danielerr@gmail.cl>
2025-10-04 12:46:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 2025DOY0101_S08_Pruebas_Lab" 
---
+++ src/test/java/cl/duoc/demomicroservicio/DemomicroservicioApplicationTests.java
@@ -0,0 +1,15 @@
+package cl.duoc.demomicroservicio;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomicroservicioApplicationTests {
+
+	//@Disabled
+	@Test
+	void contextLoads() {
+	}
+
+}
